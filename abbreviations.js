13bfc13b3dfe9bbfc6e721b1876d9419318dc1e1
Alfonsoesq <100050669+Alfonsoesq@users.noreply.github.com>
2025-07-29 21:55:41 -0600

    Create abbreviations.js

---
+++ abbreviations.js
@@ -0,0 +1,14 @@
+export const validBooks = [
+  "Génesis", "Gen", "Éxodo", "Ex", "Levítico", "Lev", "Números", "Num", "Deuteronomio", "Deut",
+  "Josué", "Jos", "Jueces", "Jue", "Rut", "1 Samuel", "1 Sam", "2 Samuel", "2 Sam",
+  "1 Reyes", "1 Rey", "2 Reyes", "2 Rey", "1 Crónicas", "1 Cro", "2 Crónicas", "2 Cro",
+  "Esdras", "Esd", "Nehemías", "Neh", "Ester", "Est", "Job", "Salmos", "Sal", "Proverbios", "Prov",
+  "Eclesiastés", "Ecl", "Cantares", "Cant", "Isaías", "Isa", "Jeremías", "Jer", "Lamentaciones", "Lam",
+  "Ezequiel", "Eze", "Daniel", "Dan", "Oseas", "Ose", "Joel", "Amós", "Abdías", "Jonás", "Miqueas",
+  "Nahúm", "Habacuc", "Sofonías", "Hageo", "Zacarías", "Malaquías", "Mateo", "Mat", "Marcos", "Mar",
+  "Lucas", "Luc", "Juan", "Hechos", "Rom", "1 Corintios", "1 Cor", "2 Corintios", "2 Cor",
+  "Gálatas", "Gal", "Efesios", "Efe", "Filipenses", "Fil", "Colosenses", "Col",
+  "1 Tesalonicenses", "1 Tes", "2 Tesalonicenses", "2 Tes", "1 Timoteo", "1 Tim", "2 Timoteo", "2 Tim",
+  "Tito", "Filemón", "Hebreos", "Heb", "Santiago", "Sant", "1 Pedro", "1 Ped", "2 Pedro", "2 Ped",
+  "1 Juan", "2 Juan", "3 Juan", "Judas", "Apocalipsis", "Apoc"
+];
